a8213a2aa1f765681f14e69d0f2d3b677c3fda15
sheshan <sheshankangokar@gmail.com>
2021-04-13 16:13:47 +0530

    add cart items utils function

--- src/components/collection/collection.component.jsx
+++ src/components/collection/collection.component.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import CollectionItem from '../collection-item/collection-item.component';
+import CollectionItem from "../collection-item/collection-item.component";
 
-import './collection.styles.scss';
+import "./collection.styles.scss";
 
 const Collection = ({ title, items }) => (
-    <div className='collection-preview'>
-        <h1 class='title'>{title.toUpperCase()}</h1>
-        <div className='preview'>
-            {
-                items
-                .filter((item, index) => index < 4)
-                .map(({id, ...otherItemProps}) => (
-                    <CollectionItem key={id} {...otherItemProps} />
-                ))
-            }
-        </div>
+  <div className="collection-preview">
+    <h1 class="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, index) => index < 4)
+        .map((item) => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
     </div>
-
+  </div>
 );
 
 export default Collection;
