abccb1b557956918d94c56c9b84c219c9321ffa0
sheshan <sheshankangokar@gmail.com>
2021-04-15 16:15:49 +0530

    added cart price feature

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-import CustomButton from '../custom-button/custom-button.component';
+import "./cart-dropdown.styles.scss";
 
-import './cart-dropdown.styles.scss';
-
-const Cart = () =>(
-    <div className='cart-dropdown'>
-        <div className='cart-items'>
-            <CustomButton>Go TO CHECKOUT</CustomButton>
-        </div>
+const CartDropDown = ({ cartItems }) => (
+  <div className="cart-dropdown">
+    <div className="cart-items">
+      {cartItems.map((cartItem) => (
+        <CartItem key={CartItem.id} item={cartItem} />
+      ))}
     </div>
-)
+    <CustomButton>Go TO CHECKOUT</CustomButton>
+  </div>
+);
 
-export default Cart;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+export default connect(mapStateToProps)(CartDropDown);
